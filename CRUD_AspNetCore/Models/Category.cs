c1915dad21401347caf24031330bc1aec9ba1ddf
SachinGhorpade <SachinGhorpade@WAIANGDESK07>
2022-07-27 17:30:53 +0530

    Add project files.

---
+++ CRUD_AspNetCore/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_AspNetCore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string Manufacturer { get; set; }
+        [Range(1, 100, ErrorMessage = "Shipping Number must be between 1 and 100 only!!")]
+        public int ShippingNo { get; set; }
+        public int RetailPrice { get; set; }
+    }
+}
